502769ba65ad9afed3335f63df4f7cc92a68700a
nllance <nllance@pm.me>
2022-02-02 17:25:48 -0500

    Data structures for jobs and queues

--- assgn1.c
+++ assgn1.c
@@ -1,137 +1,98 @@
-// Include header files; check standard folder
-// `gcc` already knows where to find these header files
-// If you wanna find them, use `locate` (or `find`)
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
-// Note: If you see something like 'abc/xyz.h', then 'xyz.h' is inside
-// a folder called 'abc'
 
-// Include header files; check local folder
-// The local folder is the space you are working out of; it is where
-// the source files are
 #include "log.h"
-
-// Note: The `#include` keyword tells the compiler to copy all the
-// contents of the header file and paste it right here.
-// (i.e. cpp assgn1.c)
-
-// The keyword `define` is a preprocessor directive. It is a constant
-// that is used as a substitution for number, strings, etc. For
-// example, `#define age 22`, tells the `cpp` to replace every
-// occurence of `age` with 22
 
 // Define priorities
 #define high    0  // 0 means that the job is high priority
 #define medium  1  // 1 = medium priority
 #define low     2  // 2 = low priority
 
-// Note: Jobs with a high priority take precedence over jobs with a
-// medium priority. Jobs with a medium priority take precedence over
-// jobs with a low priority. And by some discrete math law, jobs with
-// a high priority take precedence over jobs with a low priority
+/* Note: Jobs with a high priority take precedence over jobs with a medium priority. 
+   Jobs with a medium priority take precedence over jobs with a low priority. 
+   And by some discrete math law, jobs with a high priority take precedence over jobs with a low priority. 
+   A job in the low priority can be there for an unlimited number of times. 
+*/
 
-// The following constants define how many times a job can be in a
-// particular queue, before it is demoted
+// The following constants define how many times a job can be in a particular queue, before it is demoted. 
 #define hpq_times   2 // 2 times in the high priority queue (hpq)
 #define mpq_times   4 // 4 times in the medium priority queue (mpq)
-
-//#define TRUE  1
-//#define FALSE 0
- 
-// Note: A job in the low priority can be there for an unlimited
-// number of times
 
 // You may or may not use this; depends on your implementation
 int logindex = 0;
 int * logi = &logindex;
-// `logi` contains the address of `logindex` - this is known as pass
-// by reference, not value. 
+
+/* Data structure to handle jobs. It will have 5 members:
+  - process pid
+  - is it done or not?
+  - current priority
+  - keep track of how many times its gone through the hpq
+  - keep track of how many times its gone through the mpq
+*/
+typedef struct Job {
+  pid_t pid;
+  bool done;
+  int priority;
+  int hpq_no;
+  int mpq_no;
+} Job; 
+
+/* Data structure to handle queues. It will have 3 members:
+  - keep track of first element
+  - keep track of last element
+  - recall that there are at most 6 jobs (in total)
+  Queues are FIFO (First In, First Out)
+*/
+typedef struct Queue {
+  int queue[6];
+  int first;
+  int last;
+} Queue;
 
 // DECLARE PROTOTYPES: functions, structs, sigset, etc.
-// i.e. Prototypes for functions define the return type, name, and
-// parameters
 
 pid_t create_job(int i); // Return type is `pid_t` and parameter is an int
 void siga_handler();   // handler for signal SIGALRM
 void sigc_handler();   // handler for signal SIGCHLD
-sigset_t mymask1;      // normal signal process mask when all signals
-                       // are free but SIGALRM and SIGCHLD are blocked
+sigset_t mymask1;      // normal signal process mask when all signals are free but SIGALRM and SIGCHLD are blocked
 sigset_t mymask2;      // special signal process mask when all signals are free
-sigset_t jobmask;      // job signal process mask blocking all signals, leaving
-                       // only SIGUSR2 unblocked
+sigset_t jobmask;      // job signal process mask blocking all signals, leaving only SIGUSR2 unblocked
 struct sigaction sa_alarm;  // disposition for SIGALRM
 struct sigaction sa_chld;   // disposition for SIGCHLD
 
-// Global int which stores how many jobs are present
-// The number of jobs is supplied by the user via CLI args
+// Auxiliary functions for dealing with queues
+Queue* new_queue(); // initialize a new queue
+void enqueue(Queue* queue, int i); // add job of index i to a queue
+int dequeue(Queue* queue); // pop the first element of a queue
+
+/* Global int which stores how many jobs are present
+   The number of jobs is supplied by the user via CLI args
+*/
 int number_of_jobs;
 
-// void enqueue(??, ??);
-// int dequeue(??, ??);
-
-// TO DO
-// your own data structure(s) to handle jobs and the 3 queues
-//      1. Both will need to be implemented via structs. For example:
-//              typedef struct NAME_STRUCT {
-//                  int member1;
-//                  char member2;
-//              } NAME;
-//      2. The struct for jobs will have 5 members:
-//          - process pid
-//          - is it done or not?
-//          - current priority
-//          - keep track of how many times its gone through the hpq
-//          - keep track of how many times its gone through the mpq
-//      3. There is a maximum of 6 jobs in total, and each struct
-//         contains information for 1 job. So, how do you effectively
-//         store and manage 6 jobs at once?
-//           Hint1: If you had to declare 1000 variables of type int,
-//           how would you do it? (i.e. int x1, x2, x3, ... x1000????)
-//              Hint2: This will be your "job table"
-//      4. The struct for queue will have 3 members:
-//          - keep track of first element
-//          - keep track of last element
-//          - recall that there are at most 6 jobs (in total)
-//      5. Since we are dealing with queues, we need to be able to
-//         manipulate them; so additional functions are needed. These
-//         need to be defined in this file! Do not create another C
-//         source file; put everything in this file.
-//           Potential functions:
-//              - Enqueue (Add stuff to the queue)
-//              - Dequeue (Remove stuff from the queue)
-//              - Create  (Create the queue for the priority class)
-//           Queues are FIFO (First In, First Out)
-//         More information about implementing the queue can be found
-//         on the course website.
-//           For example:
-//              - Declaring queues
-//              - Keeping track of indices
-// your own auxialiary variables
-// END TO DO
+/* Job table
+   If there are fewer jobs than 6, the last items of the array will not be set. 
+   The index of a job used below is its index in the job table.
+*/
+Job job_table[6];
 
 // function main ------------------------------------------------- 
 int main(int argc, char** argv) {
   int i, j;
   pid_t pid;
   
-  // TO DO 
   // check the number of command line arguments, if not 2, terminate
-  // the program with a proper error message on the screen.
-  // check if the single command line argument (argv[1]) has value 3 to 6,
-  // if not, treminate the program with a proper error message on the
-  // screen.
-  // set appropriately number_of_jobs
-  // END TO DO
-  if (argc != 2) {
-    printf("ERROR: IncorrectNumberOfArgs\n");
+  // check if the single command line argument (argv[1]) has value 3 to 6, if not, terminate
+  if ((argc != 2) || (strchr("3456", argv[1]) == NULL)) {
+    printf("Error: Incorrect input\n");
     exit(0);
   }
-  printf("The argument is: %s\n", argv[1]);
-  // convert argv[1] (string) to an int
-  // the function you need is called: (_ _ _ _)
-  // store result (int) into number_of_jobs
+
+  // convert argv[1] (string) to an int and set appropriately number_of_jobs
+  number_of_jobs = atoi(argv[1]);
   
   create_log("assgn1.log");
   
@@ -239,9 +200,58 @@ int main(int argc, char** argv) {
   // END TO DO
     
   return 0;
-}// end function main
-  
-  
+} // end function main
+
+// function new_queue --------------------------------------------
+Queue* new_queue(){
+  Queue* q;
+  q->first = -1; // first == -1 represents an empty queue
+  return q;
+} // end function new_queue
+
+// function enqueue -------------------------------------------- 
+void enqueue(Queue* queue, int i) {
+  // if first < 0, set last = first = 0
+  if (queue->first < 0) {
+    queue->first = 0; 
+    queue->last = 0;
+  }
+  // if first >= 0, and last == 5, reset last = 0
+  else if (queue->last == 5) {
+    queue->last = 0;
+  }
+  // else, first >= 0 and last < 5
+  else {
+    queue->last++;
+  }
+  queue->queue[queue->last] = i; // set queue[last] = i
+} // end function enqueue
+
+// function dequeue -------------------------------------------- 
+int dequeue(Queue* queue) {
+  int index; // save the first element here
+  // if first < 0, the queue is empty, nothing to dequeue
+  if (queue->first < 0) {
+    printf("Error: This queue is empty\n");
+    exit(0);
+  }
+  // if first == last, there's only 1 element in the queue
+  else if (queue->first == queue->last) {
+    index = queue->first;
+    queue->first = -1;
+  }
+  // if first == 5, reset first = 0
+  else if (queue->first == 5) {
+    index = queue->first;
+    queue->first = 0;
+  }
+  // else, 0 <= first < 5, first++
+  else {
+    index = queue->first;
+    queue->first++;
+  }
+} // end function dequeue
+
 // function create_job -------------------------------------------- 
 pid_t create_job(int i) {
   pid_t pid;
@@ -270,9 +280,6 @@ pid_t create_job(int i) {
   return pid;
 }// end function create_job
   
-  
-  
-  
 // function siga_handler ------------------------------------------ 
 void siga_handler() {
   // TO DO
@@ -300,16 +307,3 @@ void sigc_handler() {
   // END TO DO 
     printf("%d\n",hpq_times);
 }// end function sigc_handler
-
-// TO DO
-// functions for handling your data structures
-//   For example:
-//      - Enqueue (Add to queue)
-//      - Dequeue (Remove from queue)
-//      - Create  (Create the queue; we need 1 for each priority class)
-//   More information on course website under Assignment #1
-// END TO DO
-
-//void enqueue() {
-//    // add something to queue
-//}
