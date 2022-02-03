0b47b56eee93b3b1569dce57e321c7ccd08bc9cf
nllance <nllance@pm.me>
2022-02-02 22:39:10 -0500

    main function and create_job function

--- assgn1.c
+++ assgn1.c
@@ -29,8 +29,8 @@ int * logi = &logindex;
   - process pid
   - is it done or not?
   - current priority
-  - keep track of how many times its gone through the hpq
-  - keep track of how many times its gone through the mpq
+  - keep track of how many times it's gone through the hpq
+  - keep track of how many times it's gone through the mpq
 */
 typedef struct Job {
   pid_t pid;
@@ -43,7 +43,7 @@ typedef struct Job {
 /* Data structure to handle queues. It will have 3 members:
   - keep track of first element
   - keep track of last element
-  - recall that there are at most 6 jobs (in total)
+  - a queue of length 6
   Queues are FIFO (First In, First Out)
 */
 typedef struct Queue {
@@ -53,7 +53,6 @@ typedef struct Queue {
 } Queue;
 
 // DECLARE PROTOTYPES: functions, structs, sigset, etc.
-
 pid_t create_job(int i); // Return type is `pid_t` and parameter is an int
 void siga_handler();   // handler for signal SIGALRM
 void sigc_handler();   // handler for signal SIGCHLD
@@ -75,7 +74,7 @@ int number_of_jobs;
 
 /* Job table
    If there are fewer jobs than 6, the last items of the array will not be set. 
-   The index of a job used below is its index in the job table.
+   The index of a job used elsewhere is its index in the job table.
 */
 Job job_table[6];
 
@@ -90,114 +89,98 @@ int main(int argc, char** argv) {
     printf("Error: Incorrect input\n");
     exit(0);
   }
+  printf("The argument is: %s\n", argv[1]);
 
   // convert argv[1] (string) to an int and set appropriately number_of_jobs
   number_of_jobs = atoi(argv[1]);
   
   create_log("assgn1.log");
   
-  // TO DO
-  // prepare mymask1 -- SIGCHLD and SIGALRM blocked, all other signals free 
-  // using sigemptyset(), sigfillset(), sigaddset(), sigdelset() 
-  // END TO DO
+  // prepare mymask1 -- SIGCHLD and SIGALRM blocked, all other signals free
+  if ((sigemptyset(&mymask1) == -1) ||
+      (sigaddset(&mymask1, SIGALRM) == -1) ||
+      (sigaddset(&mymask1, SIGCHLD) == -1))
+    perror("Failed to set up mymask1");
 
-  /*
-   if ((sigemptyset(&mymask1) == -1) ||
-       (sigaddset(&mymask1) SIGALRM) == -1) {
-    // dont forget to block: SIGCHLD 
-        perror("");
-    }
-  */
+  // install mymask1 as the process signal mask using sigprocmask() 
+  if (sigprocmask(SIG_SETMASK, &mymask1, NULL) == -1) 
+    perror("Failed to install mymask1 as the process mask");
 
-
-
- 
-  // TO DO
-  // instal mymask1 as the process signal mask using sigprocmask() 
-  // END TO DO 
-    if (sigprocmask(SIG_SETMASK, &mymask1, NULL) == -1) {
-        // it did fail
-    }
-  
-  // TO DO
   // prepare mymask2 -- all signals free 
-  // using sigemptyset(), sigfillset(), sigaddset(), sigdelset() 
-  // END TO DO
-  
-  // TO DO
+  if (sigemptyset(&mymask2) == -1)
+    perror("Failed to set up mymask2");
+
   // prepare jobmask -- all signals blocked except SIGUSR2 
-  // using sigemptyset(), sigfillset(), sigaddset(), sigdelset() 
-  // END TO DO
+  if ((sigfillset(&jobmask) == -1) ||
+      (sigdelset(&jobmask, SIGUSR2) == -1))
+    perror("Failed to set up jobmask");
 
-    // WRONG: sigemptyset
-    // WRONG: one by one, use sigaddset to block signals
-
-    sigfillset(&jobmask);
-    sigdelset(&jobmask, SIGUSR2);
-
-
-  // TO DO
   // prepare SIGALRM disposition sa_alarm
-  // its handler (sa_handler) is siga_handler()
-  // its signal mask (sa_mask) must block all signals
-  // its flags (sa_flags) must be set to SA_RESTART 
-  // END TO DO
+  sa_alarm.sa_handler = siga_handler; // its handler (sa_handler) is siga_handler()
+  sa_alarm.sa_flags = SA_RESTART; // its flags (sa_flags) must be set to SA_RESTART 
+  if ((sigfillset(&sa_alarm.sa_mask) == -1) || // its signal mask (sa_mask) must block all signals
+      (sigaction(SIGALRM, &sa_alarm, NULL) == -1)) // install SIGALRM disposition using sigaction() 
+    perror("Failed to install SIGALRM disposition");
   
-  // TO DO
-  // instal SIGALRM disposition using sigaction() 
-  // END TO DO
-  
-  // TO DO
   // prepare SIGCHLD disposition sa_chld
-  // its handler (sa_handler) is sigc_handler()
-  // its signal mask (sa_mask) must block all signals
-  // its flags (sa_flags) must be set to SA_RESTART 
-  // END TO DO
+  sa_chld.sa_handler = sigc_handler; // its handler (sa_handler) is sigc_handler()
+  sa_chld.sa_flags = SA_RESTART; // its flags (sa_flags) must be set to SA_RESTART 
+  if ((sigfillset(&sa_chld.sa_mask) == -1) || // its signal mask (sa_mask) must block all signals
+      (sigaction(SIGCHLD, &sa_chld, NULL) == -1)) // install SIGCHLD disposition using sigaction() 
+    perror("Failed to install SIGCHLD disposition");
 
-  // TO DO
-  // instal SIGCHLD disposition using sigaction() 
-  // END TO DO
-
-  // TO DO
-  // create empty high-priority queue
-  // create empty medium-priority queue
-  // create empty low-priority queue
-  // END TO DO
+  Queue* hpq = new_queue(); // create empty high-priority queue
+  Queue* mpq = new_queue(); // create empty medium-priority queue
+  Queue* lpq = new_queue(); // create empty low-priority queue
   
-  // TO DO
-  // create a data structure to keep information about jobs - PID, number of runs
-  // for(i = 0; i < number_of_jobs; i++) {
-  //   pid = create_job(i);
-  //    job_table[i].pid = pid // save pid for job i in your data structure
-  //    job_table[i].priority = high
-  //    job_table[i].othermember = ???
-  //   put all jobs in the high-priority queue 
-  //    
-  // }
-  // END TO DO
+  // create and save information about jobs
+  for(i = 0; i < number_of_jobs; i++) {
+    pid = create_job(i);
+    job_table[i].pid = pid; // save pid for job i
+    job_table[i].priority = high; // set priority to high
+    job_table[i].done = false; // job not finished
+    job_table[i].hpq_no = 0; // job hasn't gone through the high-priority queue
+    job_table[i].mpq_no = 0; // job hasn't gone through the medium-priority queue
+    enqueue(hpq, i); // put job in the high-priority queue
+  }
 
-  // TO DO
-  // in a loop
-  //    if all queues are empty
-  //       record it in the log by Msg("All jobs done");
-  //       and display it on the screen by msg("All jobs done");
-  //       and terminate the loop
-  //    "switch on" the first job from the highest-priority non-empty queue
-  //    by sending it the SIGUSR1 signal (using sigsend())
-  //    Record it in the log using 
-  //        Msg("Switched on high-priority job %d",job number);  or
-  //        Msg("Switched on medium-priority job %d",job number); or
-  //        Msg("Switched on low-priority job %d",job number); 
-  //    announce it on the screen using corresponding msg();
-  //    set alarm for 1 second using alarm()
-  //    switch the current signal process mask mymask1 to mymask2 while
-  //    going to suspension using sigsuspend()
-  //    (thus only SIGCHLD or SIGALRM will wake it up from suspension
-  //    SIGCHLD indicates that the job that is currently executing just
-  //    terminated, SIGALRM indicates that the time for the job currently
-  //    executing is up and it must be "switched off")
-  // end loop
-  // END TO DO
+  // loop for dispatching jobs; ends when all queues are empty
+  while(1) {
+    // the high-priority queue is not empty
+    if (hpq->first != -1){
+      // "switch on" the first job from the highest-priority non-empty queue by sending it SIGUSR1
+      kill(job_table[hpq->first].pid, SIGUSR1);
+      Msg("Switched on high-priority job %d", hpq->first); // record it in the log
+      msg("Switched on high-priority job %d", hpq->first); // display it on the screen
+    }
+    // the high-priority queue is empty but the medium-priority queue is not
+    else if (mpq->first != -1){
+      kill(job_table[mpq->first].pid, SIGUSR1);
+      Msg("Switched on medium-priority job %d", mpq->first);
+      msg("Switched on medium-priority job %d", mpq->first);
+    }
+    // the high- and medium-priority queues are empty, but the low-priority queue is not
+    else if (lpq->first != -1){
+      kill(job_table[lpq->first].pid, SIGUSR1);
+      Msg("Switched on low-priority job %d", lpq->first);
+      msg("Switched on low-priority job %d", lpq->first);
+    }
+    // all three queues are empty
+    else {
+      Msg("All jobs done");
+      msg("All jobs done");
+      break; // terminate the loop
+    }
+    alarm(1); // set alarm for 1 second
+    /* switch the current signal process mask mymask1 to mymask2 while
+       going to suspension using sigsuspend()
+       (thus only SIGCHLD or SIGALRM will wake it up from suspension
+       SIGCHLD indicates that the job that is currently executing just
+       terminated, SIGALRM indicates that the time for the job currently
+       executing is up and it must be "switched off")
+    */
+    sigsuspend(&mymask2);
+  }
     
   return 0;
 } // end function main
@@ -217,13 +200,11 @@ void enqueue(Queue* queue, int i) {
     queue->last = 0;
   }
   // if first >= 0, and last == 5, reset last = 0
-  else if (queue->last == 5) {
+  else if (queue->last == 5)
     queue->last = 0;
-  }
   // else, first >= 0 and last < 5
-  else {
+  else
     queue->last++;
-  }
   queue->queue[queue->last] = i; // set queue[last] = i
 } // end function enqueue
 
@@ -232,7 +213,7 @@ int dequeue(Queue* queue) {
   int index; // save the first element here
   // if first < 0, the queue is empty, nothing to dequeue
   if (queue->first < 0) {
-    printf("Error: This queue is empty\n");
+    printf("Error: Cannot dequeue from empty queue\n");
     exit(0);
   }
   // if first == last, there's only 1 element in the queue
@@ -250,6 +231,7 @@ int dequeue(Queue* queue) {
     index = queue->first;
     queue->first++;
   }
+  return index;
 } // end function dequeue
 
 // function create_job -------------------------------------------- 
@@ -257,33 +239,31 @@ pid_t create_job(int i) {
   pid_t pid;
   char argv0[10];
   char argv1[10];
-  
-  // TO DO
-  // switch process signal mask from mymask1 to jobmask 
-  // using sigprocmask()
-  // END TO DO
 
+  // switch process signal mask from mymask1 to jobmask 
+  if (sigprocmask(SIG_SETMASK, &jobmask, NULL) == -1) 
+    perror("Failed to switch process signal mask from mymask1 to jobmask");
   
   if ((pid = fork()) < 0) Sys_exit("fork error\n");
-  if (pid == 0) { // child process
-    strcpy(argv0,"job");
-    sprintf(argv1,"%d",i);
-    execl("job",argv0,argv1,NULL);
+
+  // child process
+  if (pid == 0) { 
+    strcpy(argv0, "job");
+    sprintf(argv1, "%d", i);
+    execl("job", argv0, argv1, NULL);
   }
-  
   // parent process
-  // TO DO
-  // switch process signal mask from jobmask back to mymask1 
-  // using sigprocmask()
-  // END TO DO 
+  else
+    // switch process signal mask from jobmask back to mymask1
+    sigprocmask(SIG_SETMASK, &mymask1, NULL);
  
   return pid;
-}// end function create_job
+} // end function create_job
   
 // function siga_handler ------------------------------------------ 
 void siga_handler() {
   // TO DO
-  // "switch of" the currently executing job by sending it SIGUSR2 signal
+  // "switch off" the currently executing job by sending it SIGUSR2 signal
   // (using kill())
   // either put the job back to the queue it came from (you must count
   // how many times it has been through the queue) or "demote it" to the
@@ -295,7 +275,7 @@ void siga_handler() {
   // announce it on the screen suing corresponding msg();
   // END TO DO 
   return;
-}// end function siga_handler
+} // end function siga_handler
   
   
 // function sigc_handler ------------------------------------------ 
@@ -306,4 +286,4 @@ void sigc_handler() {
   // Msg("job %d done",job number);
   // END TO DO 
     printf("%d\n",hpq_times);
-}// end function sigc_handler
+} // end function sigc_handler
